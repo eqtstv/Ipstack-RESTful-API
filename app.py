4e94545d0ea8b546b40e821d023bcd8871bf6e89
eqtstv <38236287+eqtstv@users.noreply.github.com>
2021-03-01 18:55:06 +0100

    Add register and login

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import sys
 import psycopg2
 import requests
 import waitress
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, make_response, request
 from flask_jwt_extended import (
     JWTManager,
     create_access_token,
     get_jwt_identity,
     jwt_required,
 )
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 IPSTACK_API_KEY = os.environ["IPSTACK_API_KEY"]
 IPSTACK_API_URL = "http://api.ipstack.com"
@@ -33,15 +35,59 @@ app.config["JWT_SECRET_KEY"] = os.environ["JWT_SECRET_KEY"]
 jwt = JWTManager(app)
 
 
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.json["username"]
+    password = request.json["password"]
+
+    if not username:
+        return make_response(jsonify({"msg": "Missing username parameter."}), 400)
+    if not password:
+        return make_response(jsonify({"msg": "Missing password parameter."}), 400)
+
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT Username, Password FROM Users WHERE Username=%s", (username,)
+    )
+
+    user = cursor.fetchall()
+
+    if user:
+        return make_response(jsonify("User aleady exists"))
+
+    cursor.execute(
+        "INSERT INTO Users (username, password) VALUES (%s, %s)",
+        (username, generate_password_hash(password, method="sha256")),
+    )
+    conn.commit()
+
+    return make_response(jsonify("User created"))
+
+
 @app.route("/login", methods=["POST"])
 def login():
+    if not request.is_json:
+        return make_response(jsonify({"msg": "Missing JSON in request."}), 400)
+
     username = request.json.get("username", None)
     password = request.json.get("password", None)
-    if username != "test" or password != "test":
-        return jsonify({"msg": "Bad username or password"}), 401
+
+    if not username:
+        return make_response(jsonify({"msg": "Missing username parameter."}), 400)
+    if not password:
+        return make_response(jsonify({"msg": "Missing password parameter."}), 400)
+
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT Username, Password FROM Users WHERE Username=%s", (username,)
+    )
+    user = cursor.fetchall()
+
+    if not user or not check_password_hash(user[0][1], password):
+        return make_response(jsonify({"msg": "Bad email or password."}), 401)
 
     access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token)
+    return make_response(jsonify(access_token=access_token), 200)
 
 
 @app.route("/protected", methods=["GET"])
@@ -58,17 +104,7 @@ def hello_world():
     return example_response
 
 
-@app.route("/db")
-def check_db():
-    cursor = conn.cursor()
-    postgreSQL_select_Query = "select * from users"
-
-    cursor.execute(postgreSQL_select_Query)
-    users = cursor.fetchall()
-    return str(users)
-
-
-@app.route("/ips/<ip_address>")
+@app.route("/ip/<ip_address>")
 def show_ip_address(ip_address):
     url = f"{IPSTACK_API_URL}/{ip_address}?access_key={IPSTACK_API_KEY}"
     response = requests.get(url).json()
